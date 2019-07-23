a4d595795cae58beb9443e701b9ea6b1af3ac652
Christophe De Blende <christophe@milieuinfo.be>
2019-07-23 18:01:57 +0200

    DSI2-324

--- vl-datepicker.src.js
+++ vl-datepicker.src.js
@@ -1,31 +1,35 @@
-import { VlElement } from '/node_modules/vl-ui-core/vl-core.js';
+import {VlElement} from '/node_modules/vl-ui-core/vl-core.js';
 
 import "/node_modules/flatpickr/dist/flatpickr.min.js";
 import "/node_modules/flatpickr/dist/l10n/nl.js";
 import "/node_modules/flatpickr/dist/l10n/fr.js";
 import "/node_modules/flatpickr/dist/l10n/de.js";
+import "/node_modules/vl-ui-input-group/vl-input-group.js";
+import "/node_modules/vl-ui-input-field/vl-input-field.js";
+import "/node_modules/vl-ui-button/vl-button.js";
+import "/node_modules/vl-ui-icon/vl-icon.js";
 
 (() => {
-    const id = 'flatpickr-style';
-    addStyle();
-    //nodig zolang de datepicker niet binnen de shadowDOM kan worden gehouden
+  const id = 'flatpickr-style';
+  addStyle();
 
+  //nodig zolang de datepicker niet binnen de shadowDOM kan worden gehouden
 
-    function addStyle() {
-        if (!document.head.querySelector('#' + id)) {
-            var style = getStyle();
-            document.head.appendChild(style);
-        }
+  function addStyle() {
+    if (!document.head.querySelector('#' + id)) {
+      var style = getStyle();
+      document.head.appendChild(style);
     }
+  }
 
-    function getStyle() {
-        var link = document.createElement('link');
-        link.setAttribute('id', id);
-        link.setAttribute('rel', 'stylesheet');
-        link.setAttribute('type', 'text/css');
-        link.setAttribute('href', '../style.css');
-        return link;
-    }
+  function getStyle() {
+    var link = document.createElement('link');
+    link.setAttribute('id', id);
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('type', 'text/css');
+    link.setAttribute('href', '../style.css');
+    return link;
+  }
 })();
 
 /**
@@ -51,177 +55,189 @@ import "/node_modules/flatpickr/dist/l10n/de.js";
  */
 export class VlDatepicker extends VlElement(HTMLElement) {
 
-    constructor() {
-        super(`
-            <style>
-                @import "../style.css";
-            </style>
-            <div id="wrapper" class="vl-input-group">
-                <input data-input
-                        id="input"
-                    class="vl-input-field vl-input-field--block"
-                    type="text"/>
-                <button data-toggle
-                        type="button"
-                        name="button"
-                        class="vl-input-addon">
-                    <i class="vl-vi vl-vi-calendar" aria-hidden="true"></i>
-                </button>
-            </div>
+  constructor() {
+    super(`
+        <style>
+            @import "../style.css";          
+        </style>
+        <vl-input-group>
+          <input 
+            id="input"
+            is="vl-input-field"
+            data-input
+            type="text"
+            block
+          />
+          <button 
+            id="button" 
+            is="vl-button" 
+            type="button" 
+            name="button"
+            class="vl-input-addon"
+            secondary 
+            data-toggle>
+              <span 
+                id="icon" 
+                is="vl-icon" 
+                icon="calendar" 
+                class="vl-vi"
+                aria-hidden="true"></span>
+          </button>
+        </vl-input-group>
         `);
+    this.__configureCommonOptions();
+  }
 
-        // defaults in lijn met Webuniversum component
-        this._options = {
-            locale: "nl",
-            dateFormat: "d-m-Y",
-            time_24hr: true,
-            wrap: true
-        };
+  static get _observedAttributes() {
+    return [
+      'type',
+      'locale',
+      'format',
+      'human-format',
+      'default-date',
+      'min-date',
+      'max-date',
+      'disabled-dates',
+      'disable-weekends',
+      'min-time',
+      'max-time',
+      'am-pm'
+    ];
+  }
 
-        //@TODO: set option to append datepicker inside shadowDOM for encapsulation, so that global CSS is not needed
-        //WARNING: causes positioning issue: https://github.com/flatpickr/flatpickr/issues/1024
-        //WARNING: may cause issues wrt z-index and stacking context
-        //this._options.appendTo = this._shadow.querySelector('#wrapper');
+  connectedCallback() {
+    this.__configureTypeSpecificOptions();
+    this.__createFlatpickr();
+  }
 
+  __configureCommonOptions() {
+// defaults in lijn met Webuniversum component
+    this._options = {
+      locale: "nl",
+      dateFormat: "d-m-Y",
+      time_24hr: true,
+      wrap: true
+    };
 
+    //@TODO: set option to append datepicker inside shadowDOM for encapsulation, so that global CSS is not needed
+    //WARNING: causes positioning issue: https://github.com/flatpickr/flatpickr/issues/1024
+    //WARNING: may cause issues wrt z-index and stacking context
+    this._options.appendTo = this._element;
+  }
 
-        /**
-         * Options specific to the type of picker
-         * supported types: date | time | date-time | multiple-dates | date-range
-         * Time range and multiple times are not supported by flatpickr
-         * multiple dates with times and date-range with times are supported by flatpickr, but not in this component, because unconventional UX
-         */
-        const type = this.getAttribute('type');
-        switch(type) {
-            case 'time':
-                this._options.enableTime = true;
-                this._options.noCalendar = true;
-                let iconClasslist = this._element.querySelector('.vl-vi-calendar').classList;
-                iconClasslist.remove('vl-vi-calendar');
-                iconClasslist.add('vl-vi-clock');
-                break;
+  /**
+   * Options specific to the type of picker
+   * supported types: date | time | date-time | multiple-dates | date-range
+   * Time range and multiple times are not supported by flatpickr
+   * multiple dates with times and date-range with times are supported by flatpickr, but not in this component, because unconventional UX
+   */
+  __configureTypeSpecificOptions() {
+    const type = this.getAttribute('type');
+    switch (type) {
+      case 'time':
+        this.__configureTimeOptions();
+        break;
 
-            case 'date-time':
-                this._options.enableTime = true;
-                break;
+      case 'date-time':
+        this.__configureDateTimeOptions();
+        break;
 
-            case 'multiple-dates':
-                this._options.mode = 'multiple';
-                break;
+      case 'multiple-dates':
+        this.__configureMultipleDatesOptions();
+        break;
 
-            case 'date-range':
-                this._options.mode = 'range';
-                break;
-
-            default:
-                break;
-        }
-
+      case 'date-range':
+        this.__configureDateRangeOptions();
+        break;
     }
+  }
 
-    connectedCallback() {
+  __configureDateRangeOptions() {
+    this._options.mode = 'range';
+  }
 
-        this.__createFlatpickr();
+  __configureMultipleDatesOptions() {
+    this._options.mode = 'multiple';
+  }
+
+  __configureDateTimeOptions() {
+    this._options.enableTime = true;
+  }
+
+  __configureTimeOptions() {
+    this._options.enableTime = true;
+    this._options.noCalendar = true;
+    this._element.querySelector('#icon').setAttribute('icon', 'clock');
+  }
+
+  attributeChangedCallback(attr, oldValue, newValue) {
+    super.attributeChangedCallback(attr, oldValue, newValue);
+    this.__createFlatpickr();
+  }
+
+  _typeChangedCallback(oldValue, newValue) {
+    console.error('The "type" attribute cannot be changed.');
+  }
+
+  _localeChangedCallback(oldValue, newValue) {
+    this._options.locale = (newValue) ? newValue : 'nl';
+  }
+
+  _formatChangedCallback(oldValue, newValue) {
+    this._options.dateFormat = (newValue) ? newValue : 'd-m-Y';
+  }
+
+  _human_formatChangedCallback(oldValue, newValue) {
+    this._options.altInput = !!(newValue);
+    this._options.altFormat = newValue;
+  }
+
+  _default_dateChangedCallback(oldValue, newValue) {
+    this._options.defaultDate = newValue;
+  }
+
+  _min_dateChangedCallback(oldValue, newValue) {
+    this._options.minDate = newValue;
+  }
+
+  _max_dateChangedCallback(oldValue, newValue) {
+    this._options.maxDate = newValue;
+  }
+
+  _min_timeChangedCallback(oldValue, newValue) {
+    this._options.minTime = newValue;
+  }
+
+  _max_timeChangedCallback(oldValue, newValue) {
+    this._options.maxTime = newValue;
+  }
+
+  _am_pmChangedCallback(oldValue, newValue) {
+    this._options.time_24hr = (!this.hasAttribute('am-pm'));
+  }
+
+  _disabled_datesChangedCallback(oldValue, newValue) {
+    this.__disableDates();
+  }
+
+  _disable_weekendsChangedCallback(oldValue, newValue) {
+    this.__disableDates();
+  }
+
+  __disableDates() {
+    const disabledDates = this.getAttribute('disabled-dates');
+    this._options.disable = (disabledDates) ? JSON.parse(disabledDates) : [];
+
+    if (this.hasAttribute('disable-weekends')) {
+      this._options.disable.push(function (date) {
+        return (date.getDay() === 0 || date.getDay() === 6)
+      });
     }
+  }
 
-
-    static get _observedAttributes() {
-        return [
-            'type',
-            'locale',
-            'format',
-            'human-format',
-            'default-date',
-            'min-date',
-            'max-date',
-            'disabled-dates',
-            'disable-weekends',
-            'min-time',
-            'max-time',
-            'am-pm'
-        ];
-    }
-
-    attributeChangedCallback(attr, oldValue, newValue) {
-        super.attributeChangedCallback(attr, oldValue, newValue);
-        this.__createFlatpickr();
-    }
-
-    _typeChangedCallback(oldValue, newValue) {
-        console.error('The "type" attribute cannot be changed.');
-    }
-
-
-    _localeChangedCallback(oldValue, newValue) {
-
-        this._options.locale = (newValue) ? newValue : 'nl';
-    }
-
-    _formatChangedCallback(oldValue, newValue) {
-
-        this._options.dateFormat = (newValue) ? newValue : 'd-m-Y';
-    }
-
-    _human_formatChangedCallback(oldValue, newValue) {
-
-        this._options.altInput = !!(newValue);
-        this._options.altFormat = newValue;
-    }
-
-    _default_dateChangedCallback(oldValue, newValue) {
-
-        this._options.defaultDate = newValue;
-    }
-
-    _min_dateChangedCallback(oldValue, newValue) {
-
-        this._options.minDate = newValue;
-    }
-
-    _max_dateChangedCallback(oldValue, newValue) {
-
-        this._options.maxDate = newValue;
-    }
-
-    _min_timeChangedCallback(oldValue, newValue) {
-
-        this._options.minTime = newValue;
-    }
-
-    _max_timeChangedCallback(oldValue, newValue) {
-
-        this._options.maxTime = newValue;
-    }
-
-    _am_pmChangedCallback(oldValue, newValue) {
-
-        this._options.time_24hr = (!this.hasAttribute('am-pm'));
-    }
-
-    _disabled_datesChangedCallback(oldValue, newValue) {
-
-        this.__disableDates();
-    }
-    _disable_weekendsChangedCallback(oldValue, newValue) {
-
-        this.__disableDates();
-    }
-
-    __disableDates(){
-
-        const disabledDates = this.getAttribute('disabled-dates');
-        this._options.disable = (disabledDates) ? JSON.parse(disabledDates) : [];
-
-        if(this.hasAttribute('disable-weekends')){
-            this._options.disable.push(function(date){
-                return (date.getDay() === 0 || date.getDay() === 6)
-            });
-        }
-    }
-
-    __createFlatpickr() {
-        this._picker = flatpickr(this._element, this._options);
-    }
+  __createFlatpickr() {
+    this._picker = window.flatpickr(this._element, this._options);
+  }
 }
 
 customElements.define('vl-datepicker', VlDatepicker);
