10e6eb8ff8a65fb8411dd144720cdfdaf75a3cb4
coemans <tom.coemans@gmail.com>
2020-02-20 08:48:35 +0100

    Stijl import enkele en dubbele quotes ondersteuning zodat de stijl ook correct geladen wordt in de build code (#88)

--- src/vl-datepicker.src.js
+++ src/vl-datepicker.src.js
@@ -34,10 +34,10 @@ export class VlDatepicker extends VlElement(HTMLElement) {
     constructor() {
         super(`
             <style>
-                @import "/style.css";
-                @import "/node_modules/vl-ui-button/style.css";
-                @import "/node_modules/vl-ui-input-field/style.css";
-                @import "/node_modules/vl-ui-icon/style.css";
+                @import '/style.css';
+                @import '/node_modules/vl-ui-button/style.css';
+                @import '/node_modules/vl-ui-input-field/style.css';
+                @import '/node_modules/vl-ui-icon/style.css';
 
                 #wrapper {
                     position: relative;
