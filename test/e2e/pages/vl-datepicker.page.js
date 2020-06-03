4eef64f7655e04180c33d1654974b99a9492f731
coemans <tom.coemans@gmail.com>
2020-06-03 11:25:30 +0200

    Introductie vl-ui-demo en ESlint (#110)

--- test/e2e/pages/vl-datepicker.page.js
+++ test/e2e/pages/vl-datepicker.page.js
@@ -1,115 +1,114 @@
 const VlDatepicker = require('../components/vl-datepicker');
-const { Page, Config } = require('vl-ui-core').Test;
-const { By } = require('vl-ui-core').Test.Setup;
-const { VlButton } = require('vl-ui-button').Test;
+const {Page, Config} = require('vl-ui-core').Test;
+const {VlButton} = require('vl-ui-button').Test;
 
 class VlDatepickerPage extends Page {
-    async _getDatepicker(selector) {
-        const datepicker = await new VlDatepicker(this.driver, selector);
-        await datepicker.scrollIntoView();
-        return datepicker;
-    }
+  async _getDatepicker(selector) {
+    const datepicker = await new VlDatepicker(this.driver, selector);
+    await datepicker.scrollIntoView();
+    return datepicker;
+  }
 
-    async _getButton(selector) {
-        return new VlButton(this.driver, selector);
-    }
+  async _getButton(selector) {
+    return new VlButton(this.driver, selector);
+  }
 
-    async getDefaultDatepicker() {
-        return this._getDatepicker('#default-datepicker');
-    }
+  async getDefaultDatepicker() {
+    return this._getDatepicker('#default-datepicker');
+  }
 
-    async getCustomFormatDatepicker() {
-        return this._getDatepicker('#custom-format-datepicker');
-    }
+  async getCustomFormatDatepicker() {
+    return this._getDatepicker('#custom-format-datepicker');
+  }
 
-    async getDefaultDateDatepicker() {
-        return this._getDatepicker('#prefilled-datepicker');
-    }
+  async getDefaultDateDatepicker() {
+    return this._getDatepicker('#prefilled-datepicker');
+  }
 
-    async getMinMaxDatepicker() {
-        return this._getDatepicker('#min-max-datepicker');
-    }
+  async getMinMaxDatepicker() {
+    return this._getDatepicker('#min-max-datepicker');
+  }
 
-    async getRangeDatepicker() {
-        return this._getDatepicker('#range-datepicker');
-    }
+  async getRangeDatepicker() {
+    return this._getDatepicker('#range-datepicker');
+  }
 
-    async getAlternatieveVisualisatieDatepicker() {
-        return this._getDatepicker('#alternatieve-visualisatie-datepicker');
-    }
+  async getAlternatieveVisualisatieDatepicker() {
+    return this._getDatepicker('#alternatieve-visualisatie-datepicker');
+  }
 
-    async getTimepicker() {
-        return this._getDatepicker('#timepicker-datepicker');
-    }
+  async getTimepicker() {
+    return this._getDatepicker('#timepicker-datepicker');
+  }
 
-    async getMinMaxTimepicker() {
-        return this._getDatepicker('#timepicker-min-max-datepicker');
-    }
+  async getMinMaxTimepicker() {
+    return this._getDatepicker('#timepicker-min-max-datepicker');
+  }
 
-    async getMeridianTimepicker() {
-        return this._getDatepicker('#timepicker-am-pm-datepicker');
-    }
+  async getMeridianTimepicker() {
+    return this._getDatepicker('#timepicker-am-pm-datepicker');
+  }
 
-    async getDateTimepicker() {
-        return this._getDatepicker('#date-time-datepicker');
-    }
+  async getDateTimepicker() {
+    return this._getDatepicker('#date-time-datepicker');
+  }
 
-    async getDotFormatDatepicker() {
-        return this._getDatepicker('#datepicker-1');
-    }
+  async getDotFormatDatepicker() {
+    return this._getDatepicker('#datepicker-1');
+  }
 
-    async clickDotFormatButton() {
-        const button = await this._getButton('#button-1');
-        await button.click();
-    }
+  async clickDotFormatButton() {
+    const button = await this._getButton('#button-1');
+    await button.click();
+  }
 
-    async getSlashFormatDatepicker() {
-        return this._getDatepicker('#datepicker-2');
-    }
+  async getSlashFormatDatepicker() {
+    return this._getDatepicker('#datepicker-2');
+  }
 
-    async clickSlashFormatButton() {
-        const button = await this._getButton('#button-2');
-        await button.click();
-    }
+  async clickSlashFormatButton() {
+    const button = await this._getButton('#button-2');
+    await button.click();
+  }
 
-    async getRangeFormatDatepicker() {
-        return this._getDatepicker('#datepicker-3');
-    }
+  async getRangeFormatDatepicker() {
+    return this._getDatepicker('#datepicker-3');
+  }
 
-    async clickRangeFormatButton() {
-        const button = await this._getButton('#button-3');
-        await button.click();
-    }
+  async clickRangeFormatButton() {
+    const button = await this._getButton('#button-3');
+    await button.click();
+  }
 
-    async getTimeFormatDatepicker() {
-        return this._getDatepicker('#datepicker-4');
-    }
+  async getTimeFormatDatepicker() {
+    return this._getDatepicker('#datepicker-4');
+  }
 
-    async clickTimeFormatButton() {
-        const button = await this._getButton('#button-4');
-        await button.click();
-    }
+  async clickTimeFormatButton() {
+    const button = await this._getButton('#button-4');
+    await button.click();
+  }
 
-    async getDateTimeFormatDatepicker() {
-        return this._getDatepicker('#datepicker-5');
-    }
+  async getDateTimeFormatDatepicker() {
+    return this._getDatepicker('#datepicker-5');
+  }
 
-    async clickDateTimeFormatButton() {
-        const button = await this._getButton('#button-5');
-        await button.click();
-    }
+  async clickDateTimeFormatButton() {
+    const button = await this._getButton('#button-5');
+    await button.click();
+  }
 
-    async getErrorDatepicker() {
-        return this._getDatepicker('#error-datepicker');
-    }
+  async getErrorDatepicker() {
+    return this._getDatepicker('#error-datepicker');
+  }
 
-    async getSuccessDatepicker() {
-        return this._getDatepicker('#success-datepicker');
-    }
+  async getSuccessDatepicker() {
+    return this._getDatepicker('#success-datepicker');
+  }
 
-    async load() {
-        return super.load(Config.baseUrl + '/demo/vl-datepicker.html');
-    }
+  async load() {
+    return super.load(Config.baseUrl + '/demo/vl-datepicker.html');
+  }
 }
 
 module.exports = VlDatepickerPage;
